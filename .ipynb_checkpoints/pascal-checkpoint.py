ed0bcb31b117c535cd2a97bfecd67d73c1ac767d
Zer02c <skillteam878@gmail.com>
2023-10-01 23:14:21 +0200

    first commit

---
+++ .ipynb_checkpoints/pascal-checkpoint.py
@@ -0,0 +1,14 @@
+import sympy
+def rechner():
+    pass
+
+
+a : int = int(input("bitte geben sie a an: "))
+b : int = int(input("bitte geben sie b an: "))
+h : int = int(input("bitte geben sie den exponent an: "))
+
+bin : int = (a + b) ** h 
+
+    
+print(bin)
+    
